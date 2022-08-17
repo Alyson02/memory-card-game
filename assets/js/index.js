55f7495d952fa8e7dfe53d0d3d0daf0eb110c399
Alyson <80166983+Alyson02@users.noreply.github.com>
2022-08-17 10:18:10 -0300

    Logica de virar a carta Startando o game

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,62 @@
-const cards = document.querySelectorAll('.card');
+//começo
 
-for(let i = 0; i < cards.length; i++){
-    let front = cards[i].querySelector('.front-face');
-    let back = cards[i].querySelector('.back-face');
+let comecoValido = false;
 
-    
+while (comecoValido == false) {
+    const numeroCartas = prompt('Digite um numero par de cartas maior que duas com no maximo 14');
+    if((numeroCartas >2 && numeroCartas <= 14) && numeroCartas % 2 == 0){
+        comecoValido = true
+    }
 }
 
+//criando cartas
+
+
+
+const cards = document.querySelectorAll(".card");
+
+let clicks = 0;
+
+cards.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    if (!card.classList.contains("selecionado") && !card.classList.contains("acertou")) click(card);
+    else console.log("já clicado");
+  });
+});
+
+function click(card) {
+  let front = card.querySelector(".front-face");
+  let back = card.querySelector(".back-face");
+
+  if (clicks < 2) {
+    card.classList.add("selecionado");
+    front.classList.add("esconder");
+    back.classList.add("mostrar");
+    clicks++;
+  }
+
+  let selecionados = document.querySelectorAll(".selecionado");
+  if (clicks === 2) {
+    if (selecionados[0].id != selecionados[1].id) {
+      console.log("errou");
+      cards.forEach((card) => {
+        if (card.classList.contains("selecionado")) {
+          setTimeout(() => {
+            card.classList.remove("selecionado");
+            card.querySelector(".front-face").classList.toggle("esconder");
+            card.querySelector(".back-face").classList.toggle("mostrar");
+          }, 1000);
+        }
+      });
+      clicks = 0;
+    } else {
+      selecionados[0].classList.remove("selecionado");
+      selecionados[0].classList.add("acertou");
+
+      selecionados[1].classList.remove("selecionado");
+      selecionados[1].classList.add("acertou");
+      console.log("acertou");
+      clicks = 0;
+    }
+  }
+}
