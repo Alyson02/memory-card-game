59f41b3bfaa3e0eb3e2c864655190241ac6f19f8
Alyson <alyson.subrim2002@gmail.com>
2022-08-17 07:43:10 -0300

    layout

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+const cards = document.querySelectorAll('.card');
+
+for(let i = 0; i < cards.length; i++){
+    let front = cards[i].querySelector('.front-face');
+    let back = cards[i].querySelector('.back-face');
+
+    
+}
+
